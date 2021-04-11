2393eaf6fa1c18a19ea50e2121384fbb8d1b415f
Nikolay Valkov <nikolay.valkov@student.manchester.ac.uk>
2021-04-11 05:35:14 +0300

    Add gradient magnitude calculation
    Horizontal and vertical images then merged to gradient magnitude. Some
    filtering applied

--- cv.py
+++ cv.py
@@ -8,7 +8,7 @@ from random import randint
 def convolution(image, weighted = False):
 
     #pad the image
-    image = cv2.copyMakeBorder(image, 1, 1, 1, 1, borderType, None, value)
+    image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_CONSTANT, None, [0, 0, 0])
 
     rows, cols = image.shape
     
@@ -20,7 +20,7 @@ def convolution(image, weighted = False):
     if not weighted:
       kernel = np.ones((3,3), np.float32) / 9
     else:
-      kernel = np.array([[0.5, 1, 0.5], [1, 2, 1], [0.5, 1, 0.5]]) / 8
+      kernel = np.array([[0.8, 1.2, 0.8], [1.2, 2, 1.2], [0.8, 1.2, 0.8]]) / 10
       
     #loop over the image and compute the values of the new smoothed image
     for i in range(1, rows - 1):
@@ -28,32 +28,68 @@ def convolution(image, weighted = False):
         for k in range(-1, 2):
           for l in range(-1, 2):
             convImage[i - 1][j - 1] += image[i + k][j + l] * kernel[k + 1][l + 1]
-        
-    #display it on the screen
+    
+    convImage = convImage.astype(np.uint8)
+    
+    #display it on the screen and save the image
     cv2.imshow("Convoluted Kitty", convImage.astype(np.uint8))
+    cv2.imwrite("smoothKitty.jpg", convImage)
+    return convImage
 
+
+def gradient(image, filename):
+    image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_CONSTANT, None, [0, 0, 0])
+    
+    rows, cols = image.shape
+    
+    tempGradientX = np.zeros((rows, cols))
+    tempGradientY = np.zeros((rows, cols))
+    gradientX = np.zeros((rows - 2, cols - 2))
+    gradientY = np.zeros((rows - 2, cols - 2))
+    gradientMagn = np.zeros((rows - 2, cols - 2))
+    
+    kernelX = np.array([-1, 0, 1])
+    kernelY = np.array([1, 2, 1])
+    
+    for i in range(1, rows - 1):
+      for j in range(1, cols - 1):
+        for k in range(-1, 2):
+          tempGradientX[i][j] += image[i][j + k] * kernelX[k + 1]
+          tempGradientY[i][j] += image[i][j + k] * kernelY[k + 1]
+          
+    for i in range(1, rows - 1):
+      for j in range(1, cols - 1):
+        for k in range(-1, 2):
+          gradientX[i - 1][j - 1] += tempGradientX[i + k][j] * kernelY[k + 1]
+          gradientY[i - 1][j - 1] += tempGradientY[i + k][j] * kernelX[k + 1]
+            
+    gradientMagn = np.power(np.power(gradientX, 2.0) + np.power(gradientY, 2.0), 0.5)
+    
+    #hist_full = cv2.calcHist([gradientMagn.astype(np.uint8)],[0],None,[256],[0,256])
+    #plt.plot(hist_full)
+    #plt.xlim([0,256])
+    #plt.show()
+    cv2.imshow("Gradient X", gradientX.astype(np.uint8))
+    cv2.imshow("Gradient Y", gradientY.astype(np.uint8))
+    cv2.imshow("Gradient Magnitude", gradientMagn.astype(np.uint8))
+        
+    cv2.imwrite(filename, gradientMagn.astype(np.uint8))
+    
+  
 img = cv2.imread("kitty.bmp", cv2.IMREAD_GRAYSCALE)
 cv2.namedWindow("Kitty Original", cv2.WINDOW_AUTOSIZE)
 cv2.namedWindow("Convoluted Kitty", cv2.WINDOW_AUTOSIZE)
-  
-top = int(0.05 * img.shape[0])  # shape[0] = rows
-bottom = top
-left = int(0.05 * img.shape[1])  # shape[1] = cols
-right = left
-borderType = cv2.BORDER_CONSTANT
+cv2.namedWindow("Gradient X", cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow("Gradient Y", cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow("Gradient Magnitude", cv2.WINDOW_AUTOSIZE)  
+
 while 1:
-      
-    value = [randint(0, 255), randint(0, 255), randint(0, 255)]
-    value = [0, 0, 0]
-    dst = cv2.copyMakeBorder(img, 1, 1, 1, 1, borderType, None, value)
     
     cv2.imshow("Kitty Original", img)
-    convolution(img, weighted = True)
+    weightedImage = convolution(img, weighted = True)
+    gradient(img, "kittyEdge.jpg")
+    #gradient(weightedImage, "smoothKittyEdge.jpg")
     c = cv2.waitKey(500)
     if c == 27:
         break
-    elif c == 99: # 99 = ord('c')
-        borderType = cv2.BORDER_CONSTANT
-    elif c == 114: # 114 = ord('r')
-        borderType = cv2.BORDER_REPLICATE
         
